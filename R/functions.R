6fdb0bfe29051974029ec594ac9d73faddd35a52
Dom Bennett <dominic.john.bennett@gmail.com>
2019-02-13 11:58:23 +0100

    first commit

---
+++ R/functions.R
@@ -0,0 +1,25 @@
+#' @name astral
+#' @title astral
+#' @description Run astral
+#' @param ... Arguments
+#' @examples
+#' library(outsider)
+#' astral <- module_import('astral', repo = 'dombennett/om..astral')
+#' astral('--help')
+#' @export
+astral <- function(...) {
+  arglist <- outsider::.arglist_get(...)
+  files_to_send <- outsider::.filestosend_get(arglist = arglist)
+  arglist <- c('-jar', '/Astral/astral.jar', arglist)
+  otsdr <- outsider::.outsider_init(repo = 'dombennett/om..astral',
+                                    cmd = 'java', arglist = arglist,
+                                    files_to_send = files_to_send, wd = getwd())
+  if ('--help' %in% arglist) {
+    # java -jar raises an error for --help
+    otsdr$ignore_errors <- TRUE
+    res <- outsider::.run(otsdr)
+  } else {
+    res <- outsider::.run(otsdr)
+  }
+  invisible(res)
+}
