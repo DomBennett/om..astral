a487c51b4bd5a51bd83c96353eee94b76124e003
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-05-09 20:42:49 +0200

    updates for new packages

--- R/functions.R
+++ R/functions.R
@@ -7,19 +7,18 @@
 #' @example /examples/astral.R
 #' @export
 astral <- function(..., java_args = NULL) {
-  astral_args <- outsider:::.arglist_get(...)
-  files_to_send <- outsider::.filestosend_get(arglist = astral_args)
-  astral_args <- outsider::.arglist_parse(arglist = astral_args)
+  astral_args <- arglist_get(...)
+  files_to_send <- filestosend_get(arglist = astral_args)
+  astral_args <- arglist_parse(arglist = astral_args)
   arglist <- c(java_args, '-jar', '/Astral/astral.jar', astral_args)
-  otsdr <- outsider::.outsider_init(repo = 'dombennett/om..astral',
-                                    cmd = 'java', arglist = arglist,
-                                    files_to_send = files_to_send, wd = getwd())
+  otsdr <- outsider_init(pkgnm = 'om..astral', cmd = 'java', arglist = arglist,
+                         files_to_send = files_to_send, wd = getwd())
   if ('--help' %in% arglist) {
     # java -jar raises an error for --help
     otsdr$ignore_errors <- TRUE
-    res <- outsider::.run(otsdr)
+    res <- run(otsdr)
   } else {
-    res <- outsider::.run(otsdr)
+    res <- run(otsdr)
   }
   invisible(res)
 }
