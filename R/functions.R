d5151d2aaa8dd142b7a772836ed1b78041a4d1fb
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-01-08 16:44:24 +0100

    switched away from arglist

--- R/functions.R
+++ R/functions.R
@@ -1,24 +1,44 @@
 #' @name astral
 #' @title astral
-#' @description Run astral
-#' @param ... Arguments for ASTRAL
-#' @param java_args Arguments for java
-#' @details Change memory settings with \code{-Xmx#M} in \code{java_args}.
+#' @description Run the Accurate Species TRee ALgorithm for inferring species
+#' trees from gene trees under the multi-species coalescent model.
+#' @param input_file File path to list of Newick trees.
+#' @param output_file File path to where resulting species tree should be saved.
+#' @param log_file File path to where log should be saved. If NULL, no log is
+#' saved.
+#' @param java_args Extra arguments for Java, character vector.
+#' @details
+#' 
+#' Online documentation for the original program can be found here
+#' \url{https://github.com/smirarab/ASTRAL}.
+#' 
+#' \code{input_file} and \code{output_file} are equivalent to the arguments
+#' '-i' and '-o', respectively.
+#' 
+#' See online for details on input_file formatting.
+#' 
+#' Change memory settings with \code{-Xmx#M} in \code{java_args}.
+#' 
 #' @example /examples/astral.R
 #' @export
-astral <- function(..., java_args = NULL) {
-  astral_args <- arglist_get(...)
-  files_to_send <- filestosend_get(arglist = astral_args)
-  astral_args <- arglist_parse(arglist = astral_args)
-  arglist <- c(java_args, '-jar', '/Astral/astral.jar', astral_args)
+astral <- function(input_file, output_file = 'astral_species.tre',
+                   log_file = NULL, java_args = NULL) {
+  arglist <- c(java_args, '-jar', '/Astral/astral.jar', '-i',
+               basename(input_file), '-o', basename(output_file))
+  if (!is.null(log_file)) {
+    arglist <- c(arglist, '2>', basename(log_file))
+  }
+  outdir <- file.path(tempdir(), 'astral_res')
+  if(!dir.exists(outdir)) {
+    dir.create(outdir)
+  }
+  on.exit(unlink(x = outdir, recursive = TRUE, force = TRUE))
   otsdr <- outsider_init(pkgnm = 'om..astral', cmd = 'java', arglist = arglist,
-                         files_to_send = files_to_send, wd = getwd())
-  if ('--help' %in% arglist) {
-    # java -jar raises an error for --help
-    otsdr$ignore_errors <- TRUE
-    res <- run(otsdr)
-  } else {
-    res <- run(otsdr)
+                         files_to_send = input_file, wd = outdir)
+  res <- run(otsdr)
+  file.copy(from = file.path(outdir, basename(output_file)), to = output_file)
+  if (!is.null(log_file)) {
+    file.copy(from = file.path(outdir, basename(log_file)), to = log_file)
   }
   invisible(res)
 }
--- examples/astral.R
+++ examples/astral.R
@@ -1,8 +1,15 @@
+# import
 library(outsider)
 astral <- module_import('astral', repo = 'dombennett/om..astral')
-astral('--help')
-# # Elaborate exmaple (NOT RUN)
-# # download example file
-# download.file(url = 'https://raw.githubusercontent.com/DomBennett/om..astral/master/test_data/song_mammals.424.gene.tre')
-# # run with test file and 1GB memmory
-# astral('-i', 'song_mammals.424.gene.tre', '-o', 'song_mammals.tre', java_args = '-Xmx1000M')
+
+# data
+# test dataset of song mammals
+url <- 'https://raw.githubusercontent.com/DomBennett/om..astral/master/test_data/song_mammals.424.gene.tre'
+input_file <- file.path(tempdir(), 'song_mammals.424.gene.tre')
+download.file(url = url, destfile = input_file)
+
+# run with test file and 1GB memmory
+output_file <- file.path(tempdir(), 'song_mammals.tre')
+log_file <- file.path(tempdir(), 'log_song_mammals.txt')
+astral(input_file = input_file, output_file = output_file, log_file = log_file,
+       java_args = '-Xmx1000M')
